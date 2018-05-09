16f520d1b7a9e6eceef0db6072dd5d9dfb7d6407
jopatino <36779409+jopatino@users.noreply.github.com>
2018-05-08 19:52:55 -0500

    Final3

---
+++ DataBinding/DataBinding/Datos.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace DataBinding
+{
+    using System.Collections.ObjectModel;
+    public class Datos:Notificable
+    {
+        private ObservableCollection<Persona> personas;
+        public ObservableCollection<Persona> Personas
+        {
+            get
+            {
+                return personas;
+            }
+            set
+            {
+                if(personas == value)
+                {
+                    return;
+                }
+                personas = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
