a44c5ddec0103c50c0ecc1789c7a264e7fc48d24
jopatino <36779409+jopatino@users.noreply.github.com>
2018-05-08 19:03:42 -0500

    Initial commit

---
+++ DataBinding/DataBinding/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBinding
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            btnEnvar.Clicked += BtnEnvar_Clicked;
+		}
+
+        private void BtnEnvar_Clicked(object sender, EventArgs e)
+        {
+            var per = (Persona)Resources["personaUno"];
+            per.Nombre = "Messi";
+            per.Pais = "Argentina";
+        }
+    }
+}
---
+++ DataBinding/DataBinding/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBinding
+{
+    class Persona
+    {
+        #region propiedades
+        public String Nombre { get; set; }
+        public String Pais { get; set; }
+        #endregion
+    }
+}
